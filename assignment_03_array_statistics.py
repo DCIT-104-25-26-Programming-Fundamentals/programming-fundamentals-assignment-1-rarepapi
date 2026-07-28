17bc697c2b6f5c9c01ecdccd65f79374b9d9f2d4
Adriel 22441358 <sapnadrieltettey@st.ug.edu.gh>
2026-07-28 14:11:38 +0000

    added find min

--- assignment_03_array_statistics.py
+++ assignment_03_array_statistics.py
@@ -60,3 +60,13 @@ def find_max(numbers):
             maximum = number
 
     return maximum
+
+def find_min(numbers):
+    minimum = numbers[0]
+
+    for number in numbers:
+        if number < minimum:
+            minimum = number
+
+    return minimum
+
