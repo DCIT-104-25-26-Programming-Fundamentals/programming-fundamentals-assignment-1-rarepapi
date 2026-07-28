16bc406b621a67ad830f7ac68ec84aac3db5cc54
Adriel 22441358 <sapnadrieltettey@st.ug.edu.gh>
2026-07-28 16:26:30 +0000

    print table

--- assignment_06_multiplication_table.py
+++ assignment_06_multiplication_table.py
@@ -55,3 +55,20 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+def print_table(number):
+    print(f"\nMultiplication Table for {number}:")
+
+    for i in range(1, 13):
+        print(f"{number} x {i} = {number * i}")
+
+
+def print_tables(n):
+    if n <= 0:
+        print("Error: Number must be greater than 0.")
+        return
+
+    for number in range(1, n + 1):
+        print_table(number)
+
+        if number != n:
+            print("---------------------------")
