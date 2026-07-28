c18e5baff4374cfd069ac117353dd3537328df31
Adriel 22441358 <sapnadrieltettey@st.ug.edu.gh>
2026-07-28 13:59:20 +0000

    added calculate average and find max

--- assignment_03_array_statistics.py
+++ assignment_03_array_statistics.py
@@ -46,3 +46,17 @@ def calculate_sum(numbers):
         total += number
 
     return total
+
+def calculate_average(numbers):
+    total = calculate_sum(numbers)
+    return total / len(numbers)
+
+
+def find_max(numbers):
+    maximum = numbers[0]
+
+    for number in numbers:
+        if number > maximum:
+            maximum = number
+
+    return maximum
