5058a0a8917b5b8531777a082d10db4a3cc8ff94
Adriel 22441358 <sapnadrieltettey@st.ug.edu.gh>
2026-07-28 17:10:53 +0000

    i added add, subtract, multiply, divide, modulus, exponent functions

--- assignment_09_simple_calculator.py
+++ assignment_09_simple_calculator.py
@@ -68,3 +68,28 @@
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
 
+def add(num1, num2):
+    return num1 + num2
+
+
+def subtract(num1, num2):
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    if num2 == 0:
+        return None
+
+    return round(num1 / num2, 2)
+
+
+def modulus(num1, num2):
+    return num1 % num2
+
+
+def exponent(num1, num2):
+    return num1 ** num2
